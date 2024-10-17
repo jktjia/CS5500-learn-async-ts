7ad28929ec8853f3fcc1f23520346240abd2e0c4
jktjia <103521429+jktjia@users.noreply.github.com>
2024-10-17 15:52:25 -0400

    in class

--- src/quiz/animations/main.ts
+++ src/quiz/animations/main.ts
@@ -13,24 +13,23 @@ const alice10 = document.querySelector<HTMLElement>("#alice1");
 const alice20 = document.querySelector<HTMLElement>("#alice2");
 const alice30 = document.querySelector<HTMLElement>("#alice3");
 
-if(alice10 && alice20 && alice30) {
-  // Promise chain  
-  alice10.animate(aliceTumbling1, aliceTiming1).finished  
-    .then(() => {
-        return alice20
-                .animate(aliceTumbling1, aliceTiming1)
-                .finished;     
-    })
-    .then(() => {
-      return alice30
-              .animate(aliceTumbling1, aliceTiming1)
-              .finished;
-    })
-    .catch((err) => alert(`Error when promising ... ${err.message}`));
+async function animate(alice : HTMLElement | null) {
+  try {
+    if (alice) {
+      await alice
+        .animate(aliceTumbling1, aliceTiming1)
+        .finished;
+    } else {
+      console.warn("#alice not found");
+    }
+  } catch (err) {
+    alert(`Error when promising ... ${err}`);
+  }
 }
-else{
-  console.warn("#alice not found");
-}
+
+animate(alice10);
+animate(alice20);
+animate(alice30);
 
 // alice10
 //     .animate(aliceTumbling1, aliceTiming1)
--- src/quiz/promise-sum-2darray.ts
+++ src/quiz/promise-sum-2darray.ts
@@ -3,8 +3,7 @@ const array2D_1 = [
     [4, 5, 6],
     [7, 8, 9]
 ];
-
-function sum1DArray(arr: number[]): Promise<number> {
+function sumRow(arr: number[]): Promise<number> {
     return new Promise(
         (resolve, reject) => {
             if(arr.length === 0) {
@@ -19,12 +18,23 @@ function sum1DArray(arr: number[]): Promise<number> {
     );
 }
 
-const rowPromises = [];
-for (let i = 0; i < array2D_1.length; i++) {
-    rowPromises.push(sum1DArray(array2D_1[i]));
+async function calculateSum(numArr: number[][]) {
+    if (numArr.length === 0) {
+        throw 'cannot calculate sum of an empty array'
+    }
+    const rowPromises = [];
+    for (let i = 0; i < array2D_1.length; i++) {
+        rowPromises.push(sumRow(array2D_1[i]));
+    }
+    try {
+        const rowSums = await Promise.all(rowPromises);
+        let sum = 0;
+        rowSums.forEach(s => sum += s)
+        console.log('sum:', sum)
+    } catch (err) {
+        console.log(err)
+    }
 }
-Promise.all(rowPromises).then(sums => {
-    let sum = 0;
-    sums.forEach(s => sum += s)
-    console.log('sum:', sum)
-}).catch(e => console.log(e));
+
+
+calculateSum(array2D_1)
